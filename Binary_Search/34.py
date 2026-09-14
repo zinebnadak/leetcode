8a49b257cd4a3b6924f10842696689a6bda4cadc
zinebnadak <zineb@nadak.ai>
2026-09-14 21:54:40 +0300

    leetcode: Binary search 34

---
+++ Binary_Search/34.py
@@ -0,0 +1,8 @@
+'''
+INSTRUCTIONS: 
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+'''
+class Solution
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
